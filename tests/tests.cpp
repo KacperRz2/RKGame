18a4cd6bd41dce24febeb059c521a94e15113c5c
KacperRz2 <kacrza@gmail.com>
2026-05-04 20:01:04 +0200

    test: add tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,9 +3,10 @@
 
 extern "C" {
     #include "../src/function.h"
+    #include "../src/macros.h"
 }
 
-TEST(GetDistanceSquaredTest, ZeroForSamePoints) {
+TEST(GetDistanceSquaredTest, ZeroForSamePoints){
     SDL_FPoint a = {0.0F, 0.0F};
     SDL_FPoint b = {0.0F, 0.0F};
     EXPECT_EQ(GetDistanceSquared(&a, &b), 0.0F);
@@ -17,7 +18,7 @@ TEST(GetDistanceSquaredTest, ZeroForSamePoints) {
     EXPECT_EQ(GetDistanceSquared(&a, &b), 0.0F);
 }
 
-TEST(GetDistanceSquaredTest, PositiveResult) {
+TEST(GetDistanceSquaredTest, PositiveResult){
     SDL_FPoint a = {1.0F, 2.0F};
     SDL_FPoint b = {3.0F, 4.0F};
     EXPECT_GT(GetDistanceSquared(&a, &b), 0.0F);
@@ -38,7 +39,7 @@ TEST(GetDistanceSquaredTest, PositiveResult) {
     EXPECT_GT(GetDistanceSquared(&a, &b), 0.0F);
 }
 
-TEST(GetDistanceSquaredTest, CorrectResult) {
+TEST(GetDistanceSquaredTest, CorrectResult){
     SDL_FPoint a = {1.0F, 2.0F};
     SDL_FPoint b = {3.0F, 4.0F};
     EXPECT_FLOAT_EQ(GetDistanceSquared(&a, &b), 8.0F);
@@ -57,4 +58,28 @@ TEST(GetDistanceSquaredTest, CorrectResult) {
     a = {1.0F, -2.0F};
     b = {3.0F, -4.0F};
     EXPECT_FLOAT_EQ(GetDistanceSquared(&a, &b), 8.0F);
+}
+
+TEST(RadToDegTest, CorrectResult){
+    EXPECT_FLOAT_EQ(RadToDeg(SDL_PI_F * 2.0F), 360.0F);
+    EXPECT_FLOAT_EQ(RadToDeg(SDL_PI_F), 180.0F);
+    EXPECT_FLOAT_EQ(RadToDeg(SDL_PI_F * 0.5F), 90.0F);
+    EXPECT_FLOAT_EQ(RadToDeg(SDL_PI_F * 0.25F), 45.0F);
+    EXPECT_FLOAT_EQ(RadToDeg(0.0F), 0.0F);
+    EXPECT_FLOAT_EQ(RadToDeg(-SDL_PI_F * 2.0F), -360.0F);
+    EXPECT_FLOAT_EQ(RadToDeg(-SDL_PI_F * 0.5F), -90.0F);
+    EXPECT_FLOAT_EQ(RadToDeg(SDL_PI_F * 3.0F), 540.0F);
+}
+
+TEST(SineSafeTest, CorrectResult){
+    SetSineCosineArrays();
+    EXPECT_FLOAT_EQ(SineSafe(SDL_PI_F * 2.0F), 0.0F);
+    EXPECT_NEAR(SineSafe(SDL_PI_F), 0.0F, MIN_ANGLE);
+    EXPECT_NEAR(SineSafe(SDL_PI_F * 0.75F), SDL_sinf(SDL_PI_F * 0.75F), MIN_ANGLE);
+    EXPECT_NEAR(SineSafe(SDL_PI_F * 0.5F), 1.0F, MIN_ANGLE);
+    EXPECT_NEAR(SineSafe(SDL_PI_F * 0.25F), SDL_sinf(SDL_PI_F * 0.25F), MIN_ANGLE);
+    EXPECT_FLOAT_EQ(SineSafe(0.0F), 0.0F);
+    EXPECT_FLOAT_EQ(SineSafe(-SDL_PI_F * 2.0F), 0.0F);
+    EXPECT_NEAR(SineSafe(-SDL_PI_F * 0.5F), -1.0F, MIN_ANGLE);
+    EXPECT_NEAR(SineSafe(SDL_PI_F * 3.0F), 0.0F, MIN_ANGLE);
 }
