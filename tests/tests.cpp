05d99de1c49f85b3634c61204284f7fe1e479ae8
KacperRz2 <kacrza@gmail.com>
2026-05-08 16:45:07 +0200

    test: add tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -99,3 +99,68 @@ TEST(SineSafeTest, CorrectResult){
     EXPECT_NEAR(SineSafe(SDL_PI_F * 3.0F), 0.0F, MIN_ANGLE);
 }
 
+TEST(CosiUnsafeTest, CorrectResult){
+    SetSineCosineArrays();
+    EXPECT_NEAR(CosiUnsafe(SDL_PI_F), -1.0F, MIN_ANGLE);
+    EXPECT_NEAR(CosiUnsafe(SDL_PI_F * 0.75F), SDL_cosf(SDL_PI_F * 0.75F), MIN_ANGLE);
+    EXPECT_NEAR(CosiUnsafe(SDL_PI_F * 0.5F), 0.0F, MIN_ANGLE);
+    EXPECT_NEAR(CosiUnsafe(SDL_PI_F * 0.25F), SDL_cosf(SDL_PI_F * 0.25F), MIN_ANGLE);
+    EXPECT_FLOAT_EQ(CosiUnsafe(0.0F), 1.0F);
+}
+
+TEST(CosiUnsafeDeathTest, BadArgument){
+    SetSineCosineArrays();
+    EXPECT_DEATH(CosiUnsafe(-SDL_PI_F * 2.0F), ".*");
+    EXPECT_DEATH(CosiUnsafe(-SDL_PI_F * 0.5F), ".*");
+}
+
+TEST(CosiSafeTest, CorrectResult){
+    SetSineCosineArrays();
+    EXPECT_FLOAT_EQ(CosiSafe(SDL_PI_F * 2.0F), 1.0F);
+    EXPECT_NEAR(CosiSafe(SDL_PI_F), -1.0F, MIN_ANGLE);
+    EXPECT_NEAR(CosiSafe(SDL_PI_F * 0.75F), SDL_cosf(SDL_PI_F * 0.75F), MIN_ANGLE);
+    EXPECT_NEAR(CosiSafe(SDL_PI_F * 0.5F), 0.0F, MIN_ANGLE);
+    EXPECT_NEAR(CosiSafe(SDL_PI_F * 0.25F), SDL_cosf(SDL_PI_F * 0.25F), MIN_ANGLE);
+    EXPECT_FLOAT_EQ(CosiSafe(0.0F), 1.0F);
+    EXPECT_FLOAT_EQ(CosiSafe(-SDL_PI_F * 2.0F), 1.0F);
+    EXPECT_NEAR(CosiSafe(-SDL_PI_F * 0.5F), 0.0F, MIN_ANGLE);
+    EXPECT_NEAR(CosiSafe(SDL_PI_F * 3.0F), -1.0F, MIN_ANGLE);
+}
+
+struct arctan2Params{
+    float a, b;
+};
+
+class arctan2Test : public testing::TestWithParam<arctan2Params>{};
+
+TEST_P(arctan2Test, CorrectResult){
+    arctan2Params p = GetParam();
+    EXPECT_NEAR(arctan2(p.a, p.b), SDL_atan2f(p.a, p.b), SDL_PI_F * 0.125F);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    arctan2Tests,
+    arctan2Test,
+    testing::Values(
+        arctan2Params{8.0F, 8.0F},
+        arctan2Params{-8.0F, -8.0F},
+        arctan2Params{8.0F, -8.0F},
+        arctan2Params{-8.0F, 8.0F},
+        arctan2Params{8.0F, 0.0F},
+        arctan2Params{0.0F, 8.0F},
+        arctan2Params{-8.0F, 0.0F},
+        arctan2Params{0.0F, -8.0F},
+        arctan2Params{8.0F, 16.0F},
+        arctan2Params{16.0F, 8.0F},
+        arctan2Params{-8.0F, 16.0F},
+        arctan2Params{16.0F, -8.0F},
+        arctan2Params{8.0F, -16.0F},
+        arctan2Params{-16.0F, 8.0F},
+        arctan2Params{-8.0F, -16.0F},
+        arctan2Params{-16.0F, -8.0F},
+        arctan2Params{0.08F, -16000.0F},
+        arctan2Params{-16000.0F, 0.08F},
+        arctan2Params{-0.08F, -16000.0F},
+        arctan2Params{-16000.0F, -0.08F}
+    )
+);
